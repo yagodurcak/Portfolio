e02fa00079f29179b3047449ac2de8a17aeee678
yagodurcak <mauridurcak@gmail.com>
2022-07-07 20:00:15 -0300

    services

--- portfolio-website/src/components/services/Services.js
+++ portfolio-website/src/components/services/Services.js
@@ -1,10 +1,121 @@
-import "./Services"
+import "./services.css"
 
+import {BiCheck} from "react-icons/bi"
 import React from 'react'
 
 function Services() {
   return (
-<section id="services"><div>Services</div></section>  )
+<section id="services">  
+    <h5 >What I Offer</h5>
+    <h2>Services</h2>
+    <div className="container services__container">
+      <div className="services__box">
+        <div className="services__head">
+          <h3>UX/UI Desing</h3>
+        </div>
+        <ul className="services__content">
+          <li>
+            <BiCheck className="services__icon" />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </div>
+      <div className="services__box">
+        <div className="services__head">
+          <h3>UX/UI Desing</h3>
+        </div>
+        <ul className="services__content">
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </div>
+      <div className="services__box">
+        <div className="services__head">
+          <h3>UX/UI Desing</h3>
+        </div>
+        <ul className="services__content">
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BiCheck className="services__icon"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+</section>  )
 }
 
 export default Services
