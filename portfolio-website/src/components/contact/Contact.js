66593f1405b61bfdfc4c07a6746ba960537ae4a3
yagodurcak <mauridurcak@gmail.com>
2022-07-10 19:03:33 -0300

    LISTO

--- portfolio-website/src/components/contact/Contact.js
+++ portfolio-website/src/components/contact/Contact.js
@@ -2,8 +2,22 @@ import "./contact.css"
 
 import {AiOutlineMail} from "react-icons/ai"
 import React from 'react'
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
 
 function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_36g16e8', 'template_6wn406f', form.current, 'kH8ZRFQ522hPVZFpE')
+
+
+    e.target.reset()
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -33,11 +47,11 @@ function Contact() {
           </div>
         </div>
         <div className="form__container">
-          <form action="" className="form__card">
-            <input type="text" placeholder="Your Full Name" />
-            <input type="email" placeholder="Your Email" />
-            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
-            <a href="" className="btn btn-primary">Send Message</a>
+          <form ref={form} className="form__card" onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="Your Full Name" />
+            <input type="email" name="email" placeholder="Your Email" />
+            <textarea name="message" id="" cols="30" rows="10"  placeholder="Your Message"></textarea>
+            <button href="" className="btn btn-primary"type="submit">Send Message</button>
           </form>
         </div>
       </div>
--- portfolio-website/src/components/footer/Footer.js
+++ portfolio-website/src/components/footer/Footer.js
@@ -1,10 +1,30 @@
 import "./footer.css"
 
+import {FaFacebookF} from "react-icons/fa"
 import React from 'react'
 
 function Footer() {
   return (
-    <section id="footer"><div>Footer</div></section>
+    <section id='footer'>
+      <div className='footer__container'>
+        <h1>MAURICIO DURCAK</h1>
+     
+          <div className='navmenu__container'>
+            <a href="" className="enlaces">Home</a>
+            <a href="" className="enlaces">About</a>
+            <a href="" className="enlaces">Experience</a>
+            <a href="" className="enlaces">Services</a>
+            <a href="" className="enlaces">Portfolio</a>
+            <a href="" className="enlaces">Testimonials</a>
+          </div>
+          <div className='icons__container'>
+            <FaFacebookF className="icon__container"/>
+            <FaFacebookF className="icon__container"/>
+            <FaFacebookF className="icon__container"/>
+          </div>
+          <p className="egator">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+    </section>
   )
 }
 
